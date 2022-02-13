b5ac4f026351d08e9b5dba4798e508f07601a124
Rustam Bikeev <rustam.bikeev@gmail.com>
2022-02-13 15:02:41 +0500

    update shopping_cart.html

--- backend/recipes/pdfrender.py
+++ backend/recipes/pdfrender.py
@@ -1,36 +1,20 @@
 import os
 
 from django.conf import settings
-from django.contrib.staticfiles import finders
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 
 
 def link_callback(uri, rel):
-    result = finders.find(uri)
-    if result:
-        if not isinstance(result, (list, tuple)):
-            result = [result]
-        result = list(os.path.realpath(path) for path in result)
-        path = result[0]
+    if uri.find(settings.MEDIA_URL) != -1:
+        path = os.path.join(settings.MEDIA_ROOT,
+                            uri.replace(settings.MEDIA_URL, ''))
+    elif uri.find(settings.STATIC_URL) != -1:
+        path = os.path.join(settings.STATIC_ROOT,
+                            uri.replace(settings.STATIC_URL, ''))
     else:
-        static_url = settings.STATIC_URL
-        static_root = settings.STATIC_ROOT
-        media_url = settings.MEDIA_URL
-        media_root = settings.MEDIA_ROOT
-
-        if uri.startswith(media_url):
-            path = os.path.join(media_root, uri.replace(media_url, ""))
-        elif uri.startswith(static_url):
-            path = os.path.join(static_root, uri.replace(static_url, ""))
-        else:
-            return uri
-
-    if not os.path.isfile(path):
-        raise Exception(
-            'media URI must start with %s or %s' % (static_url, media_url)
-        )
+        return None
     return path
 
 
